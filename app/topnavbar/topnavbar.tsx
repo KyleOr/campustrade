6c9e06c9cae70312c70c5dfa73d414c02c7b93ba
KyleOr <kyleerikoris@gmail.com>
2025-05-21 23:26:13 +1000

    checkpoint, menutoggle for navbar half works needs to revisit

--- app/topnavbar/topnavbar.tsx
+++ app/topnavbar/topnavbar.tsx
@@ -96,28 +96,51 @@ export default function TopNavbar() {
 
   const MenuToggle = () => (
     <button className={styles.menuToggle} onClick={() => setIsOpen(!isOpen)}>
-      <svg width="23" height="23" viewBox="0 0 23 23">
+      <motion.svg
+        width="23"
+        height="23"
+        viewBox="0 0 23 23"
+        initial="closed"
+        animate={isOpen ? "open" : "closed"}
+      >
         <Path
           variants={{
-            closed: { d: "M 2 2.5 L 20 2.5" },
-            open: { d: "M 3 16.5 L 17 2.5" },
+            closed: {
+              d: "M 2 2.5 L 20 2.5",
+              transition: { duration: 0.4 },
+            },
+            open: {
+              d: "M 3 16.5 L 17 2.5",
+              transition: { duration: 0.4 },
+            },
           }}
         />
         <Path
           d="M 2 9.423 L 20 9.423"
           variants={{
-            closed: { opacity: 1 },
-            open: { opacity: 0 },
+            closed: {
+              opacity: 1,
+              transition: { duration: 0.2 },
+            },
+            open: {
+              opacity: 0,
+              transition: { duration: 0.2 },
+            },
           }}
-          transition={{ duration: 0.1 }}
         />
         <Path
           variants={{
-            closed: { d: "M 2 16.346 L 20 16.346" },
-            open: { d: "M 3 2.5 L 17 16.346" },
+            closed: {
+              d: "M 2 16.346 L 20 16.346",
+              transition: { duration: 0.4 },
+            },
+            open: {
+              d: "M 3 2.5 L 17 16.346",
+              transition: { duration: 0.4 },
+            },
           }}
         />
-      </svg>
+      </motion.svg>
     </button>
   );
 
