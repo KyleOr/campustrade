dc1e504da17fa6d4b37501659401d358c4ecf628
KyleOr <kyleerikoris@gmail.com>
2025-06-05 12:11:54 +1000

    welcomeback component added

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,35 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { onAuthStateChanged, getAuth, User } from "firebase/auth";
 import styles from "./storepage.module.css";
+
+import TopNavbar from "./topnavbar/topnavbar";
 import HeroComponent from "./landingpagecomponent/herosection/herocomponent";
 import FeaturedComponent from "./landingpagecomponent/featuredsection/featuredcomponent";
-import TopNavbar from "./topnavbar/topnavbar";
 import GuideComponent from "./landingpagecomponent/guidesection/guidecomponent";
 import TrustAndServiceComponent from "./landingpagecomponent/trustandservicesection/trustandservicecomponent";
 import CallToActionComponent from "./landingpagecomponent/calltoactionsection/calltoactioncomponent";
 import FooterComponent from "./landingpagecomponent/footersection/footercomponent";
 
+import WelcomeBackComponent from "./landingpagecomponent/signedin/welcomebacksection/welcomebackcomponent";
+
 export default function StorePage() {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => unsubscribe();
+  }, []);
+
   return (
     <>
       <TopNavbar />
       <div className={styles.container}>
-        <HeroComponent />
+        {user ? <WelcomeBackComponent user={user} /> : <HeroComponent />}
         <FeaturedComponent />
         <GuideComponent />
         <TrustAndServiceComponent />
